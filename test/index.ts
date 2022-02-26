d299323b7ebcfdeed370a945ebb74a52c073ac1b
José Eduardo dos Santos <jose.eduardo.ds@gmail.com>
2022-02-26 12:05:44 -0300

    feat: typescript types for test && deploy

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,13 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Greeter__factory } from "../typechain";
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
-
+    const signers = await ethers.getSigners()
+    const greeter = await new Greeter__factory(signers[0]).deploy("Hello, world!");
     expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
+    await greeter.setGreeting("Hola, mundo!");
     expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
