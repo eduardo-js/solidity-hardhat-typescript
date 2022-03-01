f4480f703861bb0157990d11b962b3c6aedfeca4
José Eduardo dos Santos <jose.eduardo.ds@gmail.com>
2022-03-01 15:25:05 -0300

    feat: void token && testes

--- contracts/VoidToken.sol
+++ contracts/VoidToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract VoidToken is ERC20 {
-    constructor() ERC20("Void Token", "VT") {
-        _mint(msg.sender, 100000);
-    }
+  constructor() ERC20("Void Token", "VT") {
+    _mint(msg.sender, 100000);
+  }
 }
